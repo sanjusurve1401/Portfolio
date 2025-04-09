b90d536f44e61e581e20c7752aa58134ff0eb79f
Sanjeet Surve <sanju@Mac.lan>
2025-04-10 01:34:22 +0530

    intial

---
+++ src/constants/index.js
@@ -0,0 +1,229 @@
+import {
+    mobile,
+    backend,
+    creator,
+    web,
+    javascript,
+    typescript,
+    html,
+    css,
+    reactjs,
+    redux,
+    tailwind,
+    nodejs,
+    mongodb,
+    git,
+    figma,
+    docker,
+    meta,
+    starbucks,
+    tesla,
+    shopify,
+    carrent,
+    jobit,
+    tripguide,
+    threejs,
+  } from "../assets";
+  
+  export const navLinks = [
+    {
+      id: "about",
+      title: "About",
+    },
+    {
+      id: "work",
+      title: "Work",
+    },
+    {
+      id: "contact",
+      title: "Contact",
+    },
+  ];
+  
+  const services = [
+    {
+      title: "Web Developer",
+      icon: web,
+    },
+    {
+      title: "React Native Developer",
+      icon: mobile,
+    },
+    {
+      title: "Backend Developer",
+      icon: backend,
+    },
+  ];
+  
+  const technologies = [
+    {
+      name: "HTML 5",
+      icon: html,
+    },
+    {
+      name: "CSS 3",
+      icon: css,
+    },
+    {
+      name: "JavaScript",
+      icon: javascript,
+    },
+    {
+      name: "TypeScript",
+      icon: typescript,
+    },
+    {
+      name: "React JS",
+      icon: reactjs,
+    },
+    
+    {
+      name: "Tailwind CSS",
+      icon: tailwind,
+    },
+    {
+      name: "Node JS",
+      icon: nodejs,
+    },
+    {
+      name: "MongoDB",
+      icon: mongodb,
+    },
+    {
+      name: "Three JS",
+      icon: threejs,
+    },
+    {
+      name: "git",
+      icon: git,
+    },
+    {
+      name: "figma",
+      icon: figma,
+    },
+    {
+      name: "docker",
+      icon: docker,
+    },
+  ];
+  
+  const experiences = [
+    {
+      title: "Virtual Internship – Software Engineering ",
+      company_name: "Walmart Global Tech",
+      icon: starbucks,
+      iconBg: "#383E56",
+      date: "March 2025 - April 2025",
+      points: [
+        "Solved real-world challenges across logistics via Forage Virtual Experience",
+        "Bulit a custom heap in Java, UML diagram, and ERD – showcasing strong design & problem solving skills",
+      ],
+    },
+    {
+      title: "Embedded Programmer Intern",
+      company_name: "IOTLYNX TECHNOLOGIES PVT LTD",
+      icon: tesla,
+      iconBg: "#E6DEDD",
+      date: "July 2023 - Dec 2023",
+      points: [
+        "Worked on distributed IoT applications using STM32, LoRa, & C/C++ in real-time environments.",
+        "Optimized embedded code, reducing IoT device power usage by 15%.",
+        "Collaborated in an agile team to implement scalable sensor network systems.",
+      ],
+    },
+  ];
+  
+  const testimonials = [
+    {
+      testimonial:
+        "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+      name: "Sara Lee",
+      designation: "CFO",
+      company: "Acme Co",
+      image: "https://randomuser.me/api/portraits/women/4.jpg",
+    },
+    {
+      testimonial:
+        "I've never met a web developer who truly cares about their clients' success like Rick does.",
+      name: "Chris Brown",
+      designation: "COO",
+      company: "DEF Corp",
+      image: "https://randomuser.me/api/portraits/men/5.jpg",
+    },
+    {
+      testimonial:
+        "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+      name: "Lisa Wang",
+      designation: "CTO",
+      company: "456 Enterprises",
+      image: "https://randomuser.me/api/portraits/women/6.jpg",
+    },
+  ];
+  
+  const projects = [
+    {
+      name: "Travel Agent Management System",
+      description:
+        "Built a scalable travel booking platform using Java, Spring Boot, React.js & MySQL. Designed REST-APIs for user auth, booking & analytics with optimized pagination (40% faster search). Integrated Chart.js dashboard to visualize user trends & support business decisions in real-time.",
+      tags: [
+        {
+          name: "react",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "mongodb",
+          color: "green-text-gradient",
+        },
+        {
+          name: "tailwind",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: carrent,
+      source_code_link: "https://github.com/",
+    },
+    {
+      name: "Intelligent Ambulance with V2V Integration ",
+      description:
+        "Developed a smart ambulance system using Raspberry Pi & Arduino, reducing emergency response time by 30%. Built a LoRa-based traffic management system for real-time ambulance-vehicle communication.",
+      tags: [
+        {
+          name: "react",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "restapi",
+          color: "green-text-gradient",
+        },
+        {
+          name: "scss",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: jobit,
+      source_code_link: "https://github.com/",
+    },
+    {
+      name: "Deploy-LLM-App-as-API-using-LangChain-LangServe (Mini Project",
+      description:
+        "Developed a real-time ambulance system using Raspi-Pi & Arduino, applying embedded systems & reducing emergency response time by 30%. Designed LoRa-based distributed communication for V2V traffic control, enabling efficient mobility in smart city infrastructure.",
+      tags: [
+        {
+          name: "nextjs",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "supabase",
+          color: "green-text-gradient",
+        },
+        {
+          name: "css",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: tripguide,
+      source_code_link: "https://github.com/",
+    },
+  ];
+  
+  export { services, technologies, experiences, testimonials, projects };
